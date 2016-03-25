fa195ebe041fc05a63431cb98341ca4758720caf
Alexandr Murashkin <Alexandr.Murashkin@d2l.com>
2016-03-25 18:49:32 -0400

    Initial implementation

---
+++ test/support/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const config = require('./config');
+const app = express();
+
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, no-store');
+  next();
+});
+
+app.all('/echo', function(req, res){
+  res.writeHead(200, req.headers);
+  req.pipe(res);
+});
+
+app.listen(config.testPort);
